9f2a1989f7355815940f0cbbc8b939874d9dd79a
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-06-27 00:46:06 +0300

    added and changed some entities

--- HouseSale.Domain/Entities/Address.cs
+++ HouseSale.Domain/Entities/Address.cs
@@ -1,5 +1,11 @@
 ﻿namespace HouseSale.Domain.Entities;
 public class Address
 {
+    public Guid AddresId { get; set; }
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Country { get; set; }
 
+    public virtual List<House>? Houses { get; set; }    
 }
--- HouseSale.Domain/Entities/HouseImage.cs
+++ HouseSale.Domain/Entities/HouseImage.cs
@@ -1,5 +1,9 @@
 ﻿namespace HouseSale.Domain.Entities;
 public class HouseImage
 {
+    public Guid ImageId { get; set; }
+    public string ImagePath { get; set; }
 
+    public Guid HouseId { get; set; }
+    public virtual House House { get; set; }
 }
