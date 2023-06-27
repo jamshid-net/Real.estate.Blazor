239c552c825cde14223cdaf8a6b7070754d921df
Jamshid-DotNet <39515083+Jamshid999@users.noreply.github.com>
2023-06-27 11:52:09 +0500

    removed enums

--- HouseSale.Domain/PageEntities/HomePageEntity.cs
+++ HouseSale.Domain/PageEntities/HomePageEntity.cs
@@ -3,8 +3,9 @@ public class PageEntity
 {
     public Guid PageId { get; set; }
     public string Title { get; set; }
-    public string Description { get; set; }
-
+    public string? Description { get; set; }
+    public string[]? ImagesPath { get; set; }
     public string? OtherText { get; set; }
+
 
 }
