c265457531f288b84d91827228615b4edc705948
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-06-28 19:29:01 +0300

    sasa

---
+++ HouseSale.Blazor/PagesBase/RegisterBase.cs
@@ -0,0 +1,9 @@
+﻿using HouseSale.Blazor.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace HouseSale.Blazor.PagesBase;
+
+public class RegisterBase : ComponentBase
+{
+    public RegisterModels RegisterModels { get; set; } = new RegisterModels();
+}
