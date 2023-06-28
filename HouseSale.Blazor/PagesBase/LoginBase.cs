0473a41d951bd582a71e306c27c33bbb193d2735
Jamshid <39515083+jamshid-net@users.noreply.github.com>
2023-06-28 15:40:59 +0500

    Development (#20)
    * some changes (#6)
    
    * Update README.md
    
    * Update README.md
    
    * added entities (#2)
    
    Co-authored-by: @akramovdeveloper
    
    * added some entities
    
    * updated Comfort class
    
    * added and changed some entities
    
    * added some classes in application
    
    ---------
    
    Co-authored-by: akramov_developer <104636440+akramovdeveloper@users.noreply.github.com>
    Co-authored-by: Bahrom Akramov <bahrom.akramov98@gmail.com>
    
    * somchanges in classess
    
    * removed enums
    
    * removed enums
    
    * fixed domain models
    
    * Revert "Merge branch 'Development' of https://github.com/jamshid-net/Real.estate.Blazor into Development"
    
    This reverts commit 96ef44e387f8c9defbaa025066944b1ce9721629, reversing
    changes made to 8ef34dc42afe1169355d3f23e4fd0feb83a9960f.
    
    * .
    
    * added migration
    
    * added authorize
    
    * fixed login page
    
    * added login example
    
    ---------
    
    Co-authored-by: akramov_developer <104636440+akramovdeveloper@users.noreply.github.com>
    Co-authored-by: Bahrom Akramov <bahrom.akramov98@gmail.com>
    Co-authored-by: Jamshid-DotNet <39515083+Jamshid999@users.noreply.github.com>

--- HouseSale.Blazor/PagesBase/LoginBase.cs
+++ HouseSale.Blazor/PagesBase/LoginBase.cs
@@ -1,8 +1,24 @@
-﻿using Microsoft.AspNetCore.Components;
+
+using HouseSale.Blazor.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
 
 namespace HouseSale.Blazor.PagesBase;
 
 public class LoginBase:ComponentBase
 {
- 
+
+    public LoginModel LoginModel { get; set; }  = new LoginModel();
+
+
+    protected async Task OnSubmitLogin(EditContext context)
+    {
+        var LoginModel = context.Model as LoginModel;
+       
+        
+    }
+
+
+
 }
