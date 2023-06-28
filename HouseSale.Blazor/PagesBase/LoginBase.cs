f07091e2b349c4a06336924b3855af5c9aacdd6b
Jamshid-DotNet <39515083+Jamshid999@users.noreply.github.com>
2023-06-28 18:34:02 +0500

    fixed navigation

--- HouseSale.Blazor/PagesBase/LoginBase.cs
+++ HouseSale.Blazor/PagesBase/LoginBase.cs
@@ -3,22 +3,33 @@ using HouseSale.Blazor.Models;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 
-
 namespace HouseSale.Blazor.PagesBase;
 
 public class LoginBase:ComponentBase
 {
 
-    public LoginModel LoginModel { get; set; }  = new LoginModel();
+    protected NavigationManager NavigationManager { get; set; }
 
+    [Parameter]
+    public string? returnUrl { get; set; }
+
+
+    public LoginModel LoginModel { get; set; }  = new LoginModel();
+   
 
     protected async Task OnSubmitLogin(EditContext context)
     {
         var LoginModel = context.Model as LoginModel;
-       
+        if(returnUrl is null)
+        {
+            returnUrl = "/";
+        }
         
-    }
+        NavigationManager.NavigateTo($"{returnUrl}", forceLoad: false);
+        
 
+    }
+   
 
 
 }
