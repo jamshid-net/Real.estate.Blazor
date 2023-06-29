ec2dd52c312b74c13ca45ff51dc34355c34235f3
Jamshid-DotNet <39515083+Jamshid999@users.noreply.github.com>
2023-06-29 21:18:33 +0500

    started crud operations

---
+++ HouseSale.Application/UseCases/Addresses/Commands/DeleteAddressCommand.cs
@@ -0,0 +1,30 @@
+﻿using HouseSale.Application.Commons.Interfaces;
+using MediatR;
+
+namespace HouseSale.Application.UseCases.Addresses.Commands;
+public class DeleteAddressCommand : IRequest
+{
+    public Guid AddressId { get; init; }
+}
+public class DeleteAddressCommandHandler : IRequestHandler<DeleteAddressCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public DeleteAddressCommandHandler(IApplicationDbContext context)
+           => _context = context;
+
+
+    public async Task Handle(DeleteAddressCommand request, CancellationToken cancellationToken)
+    {
+        var foundAddress = await _context.Addresses.FindAsync(new object[] { request.AddressId }, cancellationToken);
+        if (foundAddress is null)
+            throw new NotFoundException();
+
+        _context.Addresses.Remove(foundAddress);
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+
+    }
+
+}
