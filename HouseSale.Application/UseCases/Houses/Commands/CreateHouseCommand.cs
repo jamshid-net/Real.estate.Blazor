a83b1e95ea606f6948dcb1bcd78aa1064042dc74
Jamshid-net <39515083+jamshid-net@users.noreply.github.com>
2023-06-30 23:20:51 +0500

    test create house form

--- HouseSale.Application/UseCases/Houses/Commands/CreateHouseCommand.cs
+++ HouseSale.Application/UseCases/Houses/Commands/CreateHouseCommand.cs
@@ -1,6 +1,7 @@
 ﻿using HouseSale.Application.Commons.Interfaces;
 using HouseSale.Application.UseCases.Addresses.Commands;
 using HouseSale.Application.UseCases.HomeSituations.Commands;
+using HouseSale.Application.UseCases.HouseImages.Commands;
 using HouseSale.Application.UseCases.LocatedNearbies.Commands;
 using HouseSale.Application.UseCases.ThereIsInHouses.Commands;
 using HouseSale.Domain.Entities;
@@ -11,25 +12,25 @@ using Microsoft.AspNetCore.Http;
 namespace HouseSale.Application.UseCases.Houses.Commands;
 public class CreateHouseCommand:IRequest
 {
-    public string Description { get; init; }
+    public string Description { get;set; }
 
-    public decimal Price { get; init; }
+    public decimal Price { get;set; }
 
-    public float Area { get; init; }
+    public float Area { get;set; }
 
-    public int CountOfRoom { get; init; }
+    public int CountOfRoom { get;set; }
 
-    public IFormFile MainImage { get; init; }
-    public List<IFormFile> HouseImages { get; init; }
+    public IFormFile MainImage { get;set; }
+    public List<IFormFile> HouseImages { get;set; } = new(15);
 
-    public CreateAddressCommand CreateAddressCommand { get; init; }
-    public Guid CategoryId { get; init; } 
+    public CreateAddressCommand CreateAddressCommand { get; set; } = new();
+    public Guid CategoryId { get;set; } 
 
-    public Guid CategoryRentSaleId { get; init; }
+    public Guid CategoryRentSaleId { get;set; }
 
-    public CreateHomeSituationCommand  CreateHomeSituationCommand { get; init; }
-    public CreateLocatedNearbyCommand CreateLocatedNearbyCommand { get;init; }
-    public CreateThereIsInHouseCommand CreateThereIsInHouseCommand { get; init; }
+    public CreateHomeSituationCommand  CreateHomeSituationCommand { get;set; }
+    public CreateLocatedNearbyCommand CreateLocatedNearbyCommand { get; set; }
+    public CreateThereIsInHouseCommand CreateThereIsInHouseCommand { get;set; }
     
 
 
@@ -47,8 +48,8 @@ public class CreateHouseCommandHandler : IRequestHandler<CreateHouseCommand>
         IWebHostEnvironment webHostEnvironment,
         IUser currentUser)
           => (_mediator, _context, _webHostEnvironment,_currentUser) = (mediator, context,webHostEnvironment,currentUser);
-       
-    
+
+
 
     public async Task Handle(CreateHouseCommand request, CancellationToken cancellationToken)
     {
@@ -58,7 +59,7 @@ public class CreateHouseCommandHandler : IRequestHandler<CreateHouseCommand>
         string filename = Guid.NewGuid() + request.MainImage.FileName;
 
         string combinedPath = Path.Combine(rootpath + @"\HouseImages", filename);
-        
+
         using (var stream = new FileStream(combinedPath, FileMode.Create))
         {
             await request.MainImage.CopyToAsync(stream);
@@ -71,7 +72,7 @@ public class CreateHouseCommandHandler : IRequestHandler<CreateHouseCommand>
             Price = request.Price,
             Area = request.Area,
             CountOfRoom = request.CountOfRoom,
-
+            MainImage = "/HouseImages/" + filename,
             AddressId = await _mediator.Send(request.CreateAddressCommand),
             CategoryId = request.CategoryId,
             CategoryRentSaleId = request.CategoryRentSaleId,
@@ -81,10 +82,35 @@ public class CreateHouseCommandHandler : IRequestHandler<CreateHouseCommand>
 
             CreatedAt = DateTime.UtcNow,
             CreatedBy = _currentUser.Id
-            
-            
 
         };
+        _context.Houses.Add(newHouse);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        var foundHouse =await _context.Houses.FindAsync(new object[] {newHouse.HouseId}, cancellationToken);
+
+        if (foundHouse is not null)
+        {
+            foreach (var photo in request.HouseImages)
+            {
+                string filename2 = Guid.NewGuid() + photo.FileName;
+                string combinedPath2 = Path.Combine(rootpath + @"\HouseImages", filename2);
+
+                using (var stream = new FileStream(combinedPath, FileMode.Create))
+                {
+                    await photo.CopyToAsync(stream);
+
+                }
+                await _mediator.Send(new CreateHouseImageCommand
+                {
+                    HouseId = foundHouse.HouseId,
+                    ImagePath = "/HouseImages/" + filename2
+                });
+
+
+            }
+        }
+        else throw new NotFoundException(nameof(House), newHouse.HouseId);
 
     }
 }
