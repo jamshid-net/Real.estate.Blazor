11e12a14fd21a1e61fc03b1c606ed03788c0332f
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-06-30 17:21:42 +0300

    completed HouseImage CRUD oprations

--- HouseSale.Application/UseCases/HouseImages/Commands/CreateHouseImageCommand.cs
+++ HouseSale.Application/UseCases/HouseImages/Commands/CreateHouseImageCommand.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HouseSale.Application.Commons.Interfaces;
+using HouseSale.Domain.Entities;
+using MediatR;
 
 namespace HouseSale.Application.UseCases.HouseImages.Commands;
-public class CreateHouseImageCommand
+public class CreateHouseImageCommand : IRequest<Guid>
 {
+    public string ImagePath { get; init; }
+    public Guid HouseId { get; init; }
+}
+
+public class CreateHouseImageCommandHandler : IRequestHandler<CreateHouseImageCommand, Guid>
+{
+    private readonly IApplicationDbContext _context;
+
+    public CreateHouseImageCommandHandler(IApplicationDbContext context)
+        => _context = context;
+    public async Task<Guid> Handle(CreateHouseImageCommand request, CancellationToken cancellationToken)
+    {
+        HouseImage houseImage = new HouseImage
+        {
+            ImagePath = request.ImagePath,
+            HouseId = request.HouseId
+        };
+        await _context.HouseImages.AddAsync(houseImage, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+        return houseImage.HouseImageId;
+    }
 }
